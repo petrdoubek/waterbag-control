cccb339ef77f22382b7420d0c78f03a71211c703
Petr Doubek <petr.doubek@deutsche-boerse.com>
2019-05-28 13:08:18 +0200

    drawing sprinkler open intervals

--- water/chart.py
+++ water/chart.py
@@ -40,56 +40,74 @@ def get_volume(db, tm_from, tm_now, tm_to):
     """returns:
        - time series [{t,stored}] printed as string
        - time series [{t,forecast}] printed as string
-       - current volume"""
+       - time series [{t,0 or CONST * MAX_VOLUME_L based on overflow closed/opened}] printed as a string
+       - current volume
+       - how many seconds is overflow opened (or -1 if closed)"""
     try:
         cursor = db.db.cursor()
 
-        query = ("SELECT time, mm FROM height"
-                 " WHERE time BETWEEN %d and %d ORDER BY time" % (tm_from, tm_now))
-        cursor.execute(query)
         stored = []
+        cursor.execute("SELECT time, mm FROM height"
+                        " WHERE time BETWEEN %d and %d ORDER BY time"
+                       % (tm_from, tm_now))
         for (sec, mm) in cursor:
             stored.append((sec, volume_l(mm)))
-        stored_string = '[' + ','.join(["{t:%d,y:%d}" % (1000*sec, l) for (sec, l) in stored]) + ']'
+        last_stored_ts = stored[-1][0]
 
-        query = ("SELECT forecast_from, forecast_to, rain_mm FROM forecast"
-                 " WHERE valid_to >= %d"
-                 "   AND (forecast_from BETWEEN %d and %d"
-                 "        OR forecast_to BETWEEN %d and %d)"
-                 " ORDER BY forecast_from" % (tm_now, tm_now, tm_to, tm_now, tm_to))
-        cursor.execute(query)
-        forecast = [(stored[-1][0], stored[-1][1])]
+        cursor.execute("SELECT forecast_from, forecast_to, rain_mm FROM forecast"
+                        " WHERE valid_to >= %d"
+                        "   AND (forecast_from BETWEEN %d and %d"
+                        "        OR forecast_to BETWEEN %d and %d)"
+                        " ORDER BY forecast_from"
+                       % (tm_now, tm_now, tm_to, tm_now, tm_to))
+        forecast = [(last_stored_ts, stored[-1][1])]
         cumsum_l = stored[-1][1]
         for (from_s, to_s, mm) in cursor:
             cumsum_l += rain_l(mm)
             forecast.append((int((from_s + to_s)/2), cumsum_l))
-        if len(forecast) >= 2 and forecast[1][0] < stored[-1][0]:
-            forecast[1] = (stored[-1][0], forecast[1][1])  # do not go back in time with first forecast
+        if len(forecast) >= 2 and forecast[1][0] < last_stored_ts:
+            forecast[1] = (last_stored_ts, forecast[1][1])  # do not go back in time with first forecast
             # TODO forecast only proportionate part of rain?
-        forecast_string = '[' + ','.join(["{t:%d,y:%d}" % (1000*sec, l) for (sec, l) in forecast]) + ']'
 
-        overflow_s = -1
-        query = "SELECT time, msg FROM log WHERE msg LIKE 'overflow_%' ORDER BY time DESC LIMIT 1"
-        cursor.execute(query)
+        overflow = []
+        cursor.execute("SELECT time, msg FROM log"
+                        " WHERE time BETWEEN %d and %d"
+                        "   AND msg LIKE 'overflow_%%'" 
+                        " ORDER BY time"
+                       % (tm_from, tm_now))
         for (log_ts, msg) in cursor:
-            if msg.startswith('overflow_opened'):
-                overflow_s = tm_now - log_ts;
-            break
+            if msg.startswith('overflow_opened'):  # going up
+                overflow.append((log_ts, 0))
+                overflow.append((log_ts, MAX_VOLUME_L/6))
+            if msg.startswith('overflow_closed'):  # going down
+                overflow.append((log_ts, MAX_VOLUME_L/6))
+                overflow.append((log_ts, 0))
+        if len(overflow) > 0:
+            overflow.append((last_stored_ts, overflow[-1][1]))
 
         cursor.close()
-        return (stored_string, forecast_string, stored[-1][1], overflow_s)
+        return (timeseries_csv(stored), timeseries_csv(forecast), timeseries_csv(overflow),
+                stored[-1][1],
+                tm_now - overflow[-1][0] if len(overflow) > 0 and overflow[-1][1]>0 else -1)
     except mysql.connector.Error as err:
         return err.msg
         # TODO error handling!
 
 
+def timeseries_csv(stored):
+    stored_string = '[' + ','.join(["{t:%d,y:%d}" % (1000 * sec, l) for (sec, l) in stored]) + ']'
+    return stored_string
+
+
 def html_chart(db):
     tm_now = time.time()
-    (stored, forecasted_rain, now_l, overflow_s) = get_volume(db, tm_now - INTERVAL_PAST_S, tm_now, tm_now + INTERVAL_FUTURE_S)
+    (stored, forecasted_rain, overflow, now_l, overflow_s) = \
+        get_volume(db, tm_now - INTERVAL_PAST_S, tm_now, tm_now + INTERVAL_FUTURE_S)
     with open(CHART_TEMPLATE, 'r') as template_file:
         return template_file.read()\
             .replace('%STATE%', '%dl, %s' % (now_l, ('overflow open %ds' % overflow_s) if overflow_s>=0 else 'overflow closed'))\
-            .replace('%STORED%', stored)\
+            .replace('%STORED%', stored) \
+            .replace('%OVERFLOW%', overflow) \
             .replace('%FORECASTED_RAIN%', forecasted_rain)\
             .replace('%MAX_L%', '%d' % round(1.5*MAX_VOLUME_L))\
             .replace('%PARAMETERS%', li_parameters())

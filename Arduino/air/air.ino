da33c2b2b2c32cbbe04c76301d2d2a6a2811e5fd
petrdoubek <petr.doubek@deutsche-boerse.com>
2020-04-28 22:47:03 +0200

    some changes

--- Arduino/air/air.ino
+++ Arduino/air/air.ino
@@ -1,12 +1,15 @@
 /*
-    Measuring air temperature and humidity and soil moisture, sending over WiFi to a server
+    Measuring air temperature and humidity (DHT sensor) and soil moisture, sending over WiFi to a server
+
+    uses deep sleep, D0 and RST pins must be connected after the sketch is uploaded, otherwise it never wakes up
 */
 
 #include <DHT.h>;
 
 #define DHT_PIN  D7     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHT_PIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+DHT dht1(DHT_PIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+DHT dht2(D1, DHTTYPE);
 
 #define SOIL_MEASURE_PIN A0     // soil moisture sensor (optional)
 #define SOIL_VIN_PIN     D6     // soil voltage pin - turn it on only for measurement
@@ -18,7 +21,7 @@ DHT dht(DHT_PIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
 #include "JsonConfig.h"
 JsonConfig jcfg;
 
-//#define USE_DISPLAY // optional, use 4 digit TM1637 display for debugging
+#define USE_DISPLAY // optional, use 4 digit TM1637 display for debugging
 #define DIO_PIN          D3  // display - optional
 #define CLK_PIN          D4  // display - optional
 #include "Display4Digit.h"
@@ -29,31 +32,30 @@ Display4Digit disp4(CLK_PIN, DIO_PIN);
 WiFiClientHTTPS wific(WIFI_SSID, WIFI_PASSWORD, SERVER, &disp4);
 
 #include <MedianFilterLib.h>
-#define WINDOW 25 // median filter window fixed, easier than configurable
+#define WINDOW 9 // median filter window fixed, easier than configurable
 MedianFilter<float> medianFilterTemp(WINDOW);
 MedianFilter<int> medianFilterHumidity(WINDOW);
 MedianFilter<int> medianFilterMoisture(WINDOW);
 
-float last_sent_temp = 100000.0, last_sent_moisture = 100000;
-int till_measure_s, till_send_s, till_force_send_s;
-bool measured = false;
+bool measured_temperature = false, measured_humidity = false, measured_moisture = false;
 
 
 void init_config() {
-  jcfg.val["MIN_CHANGE_C"] = 1.0;
-  jcfg.val["MIN_CHANGE_RES"] = 50;
-  jcfg.val["CYCLE_MEASURE_S"] = 240;
-  jcfg.val["CYCLE_SEND_S"] = 3600;
-  jcfg.val["FORCE_SEND_S"] = 4 * 3600;
-  jcfg.val["WIFI_TIMEOUT_S"] = 30;
+  jcfg.val["CYCLE_SEND_S"] = 10;
+  jcfg.val["WIFI_TIMEOUT_S"] = 10;
 }
 
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
+  Serial.setTimeout(2000);
+  while(!Serial) {}
+  
+  dht1.begin();
+  dht2.begin();
+#ifdef SOIL_VIN_PIN
   pinMode(SOIL_VIN_PIN, OUTPUT);
-  soilSensorSwitch(LOW);
+#endif
 
   init_config();
 
@@ -68,83 +70,81 @@ void setup() {
 #endif
   Serial.print("using config: ");
   jcfg.printMe();
-
-  till_measure_s = 1;
-  till_send_s = jcfg.val["CYCLE_SEND_S"];
-  till_force_send_s = jcfg.val["FORCE_SEND_S"];
 }
 
 
 void loop() {
-  if (till_measure_s <= 0) {
-    measure();
-    till_measure_s = jcfg.val["CYCLE_MEASURE_S"];
+  soilSensorSwitch(HIGH); // send current through the soil only for necessary time to save battery
+  for (int i=0; i<WINDOW; i++) {
+    Serial.printf("#%03d ", i);
+    measure_dht(dht1);
+    measure_soil();
+    Serial.println();
+    delay(2000); // DHT22 requires 2s interval between measurements
   }
+  soilSensorSwitch(LOW);
+  send_to_server();
+  disp4.off();
 
-  if (till_send_s <= 0) {
-    if (measured) {
-      float filtered_temp = medianFilterTemp.GetFiltered();
-      float filtered_humidity = medianFilterHumidity.GetFiltered();
-      float filtered_moisture = medianFilterMoisture.GetFiltered();
-      if (till_force_send_s <= 0
-          || abs(last_sent_temp - filtered_temp) >= (float) jcfg.val["MIN_CHANGE_C"]
-          || abs(last_sent_moisture - filtered_moisture) >= (int) jcfg.val["MIN_CHANGE_RES"]) {
-        if (insert_environment(filtered_temp, filtered_humidity, filtered_moisture)) {
-          last_sent_temp = filtered_temp;
-          last_sent_moisture = filtered_moisture;
-          till_force_send_s = jcfg.val["FORCE_SEND_S"];
-        } else {
-          Serial.println("sending failed");
-        }
-        //load_command();  // check if server has any task (new config etc.)
-      } else {
-        Serial.printf("sending skipped, change < %.1fC\n", (float) jcfg.val["MIN_CHANGE_C"]);
-      }
-    } else {
-      Serial.println("sending skipped, nothing measured so far");
-    }
-    till_send_s = jcfg.val["CYCLE_SEND_S"];
-  }
-  int skip_s = min(till_measure_s, till_send_s);
-  till_measure_s -= skip_s;
-  till_send_s -= skip_s;
-  till_force_send_s -= skip_s;
-  delay(1000 * skip_s);
+  Serial.printf("Going into deep sleep for %ds\n", (long) jcfg.val["CYCLE_SEND_S"]);
+  //ESP.deepSleep(1000000l * (long) jcfg.val["CYCLE_SEND_S"]);
+  // there will be only one loop, the sketch restarts at setup after deep sleep
+  delay(1000 * (long) jcfg.val["CYCLE_SEND_S"]);
 }
 
 
-void measure() {
+void send_to_server() {
+  if (measured_temperature || measured_moisture) {
+    float filtered_temp = medianFilterTemp.GetFiltered();
+    float filtered_humidity = medianFilterHumidity.GetFiltered();
+    float filtered_moisture = medianFilterMoisture.GetFiltered();
+    if (!insert_environment(filtered_temp, filtered_humidity, filtered_moisture)) {
+      Serial.println("sending failed");
+    }
+    //load_command();  // check if server has any task (new config etc.)
+  } else {
+    Serial.println("sending skipped, nothing measured so far");
+  }
+}
+
+
+void measure_dht(DHT dht) {
   int hum_pct = (int) dht.readHumidity();
   float temp_C = dht.readTemperature();
-#ifdef SOIL_MEASURE_PIN
-  soilSensorSwitch(HIGH);
-  delay(500);  // send current to the sensor only for a short time
-  int soil_resistance = analogRead(SOIL_MEASURE_PIN);
-  soilSensorSwitch(LOW);
-  medianFilterMoisture.AddValue(soil_resistance);
-#endif
 
-  //if (temp_C >= -50.0 && temp_C <= 100.0 && hum_pct >= 0 && hum_pct <= 100) {
-  if (true) {
-    Serial.printf("measurement: Humidity: %3d%%, Temp: %5.1fC", hum_pct, temp_C);
-#ifdef SOIL_MEASURE_PIN
-    Serial.printf(", Soil resistance: %4d", soil_resistance);
-#endif
-    Serial.println();
+  Serial.printf("    Temp: %5.1fC", temp_C);
+  if (temp_C >= -50.0 && temp_C <= 100.0) {
     medianFilterTemp.AddValue(temp_C);
-    medianFilterHumidity.AddValue(hum_pct);
-    Serial.printf("  median %3d%% %5.1fC", medianFilterHumidity.GetFiltered(), medianFilterTemp.GetFiltered());
-#ifdef SOIL_MEASURE_PIN
-    Serial.printf(" %4d", medianFilterMoisture.GetFiltered());
-#endif
-    Serial.println();
-#ifdef USE_DISPLAY
+    measured_temperature = true;
     disp4.showNumberDec((int) temp_C, false);
-#endif
-    measured = true;
   } else {
-    disp4.printDispErr("measurement out of range", 5);
+    disp4.printDispErr("temperature out of range", 5);
   }
+
+  Serial.printf("    Humidity: %3d%%", hum_pct);
+  if (hum_pct >= 0 && hum_pct <= 100) {
+    medianFilterHumidity.AddValue(hum_pct);
+    measured_humidity = true;
+  } else {
+    disp4.printDispErr("humidity out of range", 6);
+  }
+
+  Serial.printf("    Dew point: %5.1fC", dew_point(temp_C, (float) hum_pct / 100.0));
+}
+
+
+void measure_soil() {
+#ifdef SOIL_MEASURE_PIN
+  int soil_resistance = analogRead(SOIL_MEASURE_PIN);
+
+  Serial.printf("    Soil resistance: %4d", soil_resistance);
+  if (soil_resistance > 0 && soil_resistance <= 1024) {
+    medianFilterMoisture.AddValue(soil_resistance);
+    measured_moisture = true;
+  } else {
+    disp4.printDispErr("soil resistance out of range", 7);
+  }
+#endif
 }
 
 
@@ -158,13 +158,18 @@ void soilSensorSwitch(int value) {
 bool insert_environment(float temp, int hum, int mois) {
   String ignored_response;
   return wific.get_url(String(INSERT_PATH)
-                       + "insert_temperature=" + String(temp, 1)
-                       + "&insert_humidity=" + String(hum)
-#ifdef SOIL_MEASURE_PIN
-                       + "&insert_moisture=" + String(mois)
-#endif
-                       ,
-                       ignored_response, true, (int) jcfg.val["WIFI_TIMEOUT_S"]);
+           + (measured_temperature ? ("&insert_temperature=" + String(temp, 1)) : "")
+           + (measured_humidity ? ("&insert_humidity=" + String(hum)) : "")
+           + (measured_moisture ? ("&insert_moisture=" + String(mois)) : ""),
+           ignored_response, true, (int) jcfg.val["WIFI_TIMEOUT_S"]);
+}
+
+
+/* calculate dew point [C] given temperature T [C] and humidity h [0-1] */
+float dew_point(float T, float h) {
+  float exp_term = (17.67 * T) / (243.5 + T);
+  float ln_term = log(h * exp(exp_term));
+  return (243.5 * ln_term) / (17.67 + ln_term);
 }
 
 /*
